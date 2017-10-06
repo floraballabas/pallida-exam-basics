270534db76e0c7076bf03f85a03f9ac3c0030dde
Flora <floraballabas@gmail.com>
2017-10-06 10:59:22 +0200

    Created the classes for the exercise

---
+++ candyshop/CandyShop/CandyShop/Sweets.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyShopApp
+{
+    public class Sweets
+    {
+        protected double price;
+        protected double sugar;
+
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+        public double Sugar
+        {
+            get
+            {
+                return sugar;
+            }
+            set
+            {
+                sugar = value;
+            }
+        }
+    }
+}
