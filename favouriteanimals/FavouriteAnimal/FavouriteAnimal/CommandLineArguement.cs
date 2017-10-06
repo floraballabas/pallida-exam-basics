b625857901f4247ec7d86108a778ad2fd2f731b4
Flora <floraballabas@gmail.com>
2017-10-06 10:40:19 +0200

    Created the command line arguement to add new animals to new lines in the text file

---
+++ favouriteanimals/FavouriteAnimal/FavouriteAnimal/CommandLineArguement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FavouriteAnimal
+{
+    class CommandLineArguement
+    {
+        public void AddFavouriteAnimal(string animalToStore)
+        {
+            try
+            {
+                using (StreamWriter file = File.AppendText(@"../../favourites.txt"))
+                {
+                    file.WriteLine(animalToStore);
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Unable to add: no animal provided");
+            }
+        }
+        public void RunWithoutArguement()
+        {
+            Console.WriteLine("C# FavouriteAnimals [animal] [animal]");
+        }
+    }
+}
